f8800f49395832989dc8db9ba027500f17060452
Diego785 <diegohurtado2412@gmail.com>
2023-05-19 11:47:01 -0400

    fixed

--- lib/global/enviroment.dart
+++ lib/global/enviroment.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
 class Environment {
-  static String apiUrl = 'http://192.168.132.16/alerta-temprana-scz/public/api';
+  // static String apiUrl = 'http://192.168.132.16/alerta-temprana-scz/public/api';
+   static String apiUrl = 'http:// 192.168.132.43/alerta-temprana-scz/public/api';
+
 }
